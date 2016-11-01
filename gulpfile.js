b9d24fbc60c4793b3a522f03d43af772740b98fc
Fred Chasen <fchasen@gmail.com>
2016-11-01 14:17:02 +0100

    updated gulpfile, built documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 var documentation = require('./'),
   gulp = require('gulp');
 
-gulp.task('documentation', function () {
+gulp.task('documentation', ["documentation:md", "documentation:html", "documentation:json"]);
 
-  gulp.src('./index.js')
+gulp.task('documentation:md', function () {
+
+  return gulp.src('./index.js')
     .pipe(documentation({ format: 'md' }))
     .pipe(gulp.dest('md-documentation'));
 
-  gulp.src('./index.js')
+});
+
+gulp.task('documentation:html', function () {
+
+  return gulp.src('./index.js')
     .pipe(documentation({ format: 'html' }))
     .pipe(gulp.dest('html-documentation'));
 
-  gulp.src('./index.js')
+});
+
+gulp.task('documentation:json', function () {
+
+  return gulp.src('./index.js')
     .pipe(documentation({ format: 'json' }))
     .pipe(gulp.dest('json-documentation'));
 
